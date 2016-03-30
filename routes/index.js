67559624cc0fcf4f188f01fc9964af1f85600cb4
Timothy Huesmann <timothy.huesmann@gmail.com>
2016-03-29 20:58:12 -0500

    finished the create account

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,9 @@ router.get('/login/:username/:region', function(req,res,next)
 	}
 	else
 	{
+		var lowerUser = tempUsername.toLowerCase();
+		var summonerID = jsonObj[lowerUser].id;
+		console.log(summonerID);
 		usersRef.on("value", function(snapshot){		//Checking to see if username exists in Firebase already
 		if(snapshot == null)	//Firebase has an error
 		{
@@ -43,11 +46,72 @@ router.get('/login/:username/:region', function(req,res,next)
 		
 			if(snapshot.child(tempUsername).exists() == true) //username exists in the database -> proceed to login
 			{
-				
+				var region = tempRegion.toLowerCase();
+
 			}
 			else //username doesn't exist in databse, create user account then login
 			{
-				
+				var region = tempRegion.toLowerCase();
+				var query2 = 'https://global.api.pvp.net/api/lol/static-data/'+region+'/v1.2/champion?api_key='+apiKey;
+				console.log(query2);
+				var champJSON = JSON.parse(Get(query2));
+				if(champJSON.status != null)
+				{
+					console.log("Error");					//No data found or error (most likely an error)
+					console.log(champJSON);
+				}
+				else
+				{	
+					var championString = '{"Champion Data":{';
+					for(var i = 0; i<Object.keys(champJSON.data).length;i++)	//starts the JSON string creation
+					{
+						championString+= '"' + Object.keys(champJSON.data)[i] + '"' + ':{"with":{';
+						for(var j=0; j<Object.keys(champJSON.data).length;j++)   //builds the "Playing with" part of the JSON String
+						{
+							championString+= '"' + Object.keys(champJSON.data)[j] + '"' + ':"0-0"';
+							if(j != Object.keys(champJSON.data).length-1)
+							{
+								championString+=',';
+							}
+						}
+						championString+= '}, "against":{';
+						for(var k=0; k<Object.keys(champJSON.data).length;k++) //builds the "playing against" part of the JSON String
+						{
+							championString+= '"' + Object.keys(champJSON.data)[k] + '"' + ':"0-0"';
+							if(k != Object.keys(champJSON.data).length-1)
+							{
+								championString+=',';
+							}
+						}
+						championString+='}}';
+						if(i != Object.keys(champJSON.data).length-1)
+						{
+							championString+=',';
+						}
+						
+					}
+
+					championString+= '}}';
+
+					var query3 = 'https://global.api.pvp.net/api/lol/' + tempRegion.toLowerCase() + '/v1.3/stats/by-summoner/' + summonerID + '/summary?api_key=' + apiKey;
+					var statsJSON = JSON.parse(Get(query3));
+					console.log(statsJSON.playerStatSummaries);
+					var totalWins = parseInt(statsJSON.playerStatSummaries[7].wins) + parseInt(statsJSON.playerStatSummaries[9].wins);
+					var totalKills = parseInt(statsJSON.playerStatSummaries[7].aggregatedStats.totalChampionKills) + parseInt(statsJSON.playerStatSummaries[9].aggregatedStats.totalChampionKills);
+					var totalCS = parseInt(statsJSON.playerStatSummaries[7].aggregatedStats.totalMinionKills) + parseInt(statsJSON.playerStatSummaries[7].aggregatedStats.totalNeutralMinionsKilled) + parseInt(statsJSON.playerStatSummaries[9].aggregatedStats.totalMinionKills) + parseInt(statsJSON.playerStatSummaries[9].aggregatedStats.totalNeutralMinionsKilled);
+					var totalTurrets = parseInt(statsJSON.playerStatSummaries[7].aggregatedStats.totalTurretsKilled) + parseInt(statsJSON.playerStatSummaries[9].aggregatedStats.totalTurretsKilled);
+					var totalAssists = parseInt(statsJSON.playerStatSummaries[7].aggregatedStats.totalAssists) + parseInt(statsJSON.playerStatSummaries[9].aggregatedStats.totalAssists);
+					
+					statisticsString = '{"stats": {"wins":'+ totalWins + ',"kills":'+totalKills+',"cs":' + totalCS+',"totalTurrets":'+totalTurrets+',"totalAssists":'+totalAssists+'}}';
+					
+					usersRef.child(tempUsername).set({
+							championData: championString,
+							region: tempRegion,
+							statistics: statisticsString	
+					});
+					
+
+				}
 			}
 			
 		}
@@ -66,17 +130,7 @@ function Get(yourUrl) {
 
 function createFirebaseAccount(username, region)
 {
-	var query = 'https://global.api.pvp.net/api/lol/static-data/' + region + '/v1.2/champion?api_key=' + apiKey;
-	var champJSON = JSON.parse(Get(query));
-	if(champJSON.status != null)
-	{
-		console.log("Error");					//No data found or error (most likely an error)
-		console.log(champJSON);
-	}
-	else
-	{
-		console.log(champJSON.data);
-	}
+	
 }
 
 module.exports = router;
